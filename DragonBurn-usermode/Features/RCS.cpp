8a59bdc603faab2dbf79a3b26a733bb07a0a9406
valerobrennan <valero.brennan@gmail.com>
2025-11-04 17:37:14 -0600

    Update RCS.cpp

--- DragonBurn-usermode/Features/RCS.cpp
+++ DragonBurn-usermode/Features/RCS.cpp
@@ -1,80 +1,88 @@
 #include "RCS.h"
 
-void RCS::UpdateAngles(const CEntity& Local, Vec2& Angles)
-{
-	auto oldPunch = Vec2{ };
-	auto shotsFired = Local.Pawn.ShotsFired;
-
-	int ScreenCenterX = Gui.Window.Size.x / 2;
-	int ScreenCenterY = Gui.Window.Size.y / 2;
-
-	if (shotsFired)
-	{
-		uintptr_t clientState;
-		auto viewAngles = Local.Pawn.ViewAngle;
-		auto aimPunch = Local.Pawn.AimPunchAngle;
-
-		auto newAngles = Vec2
-		{
-			viewAngles.x + oldPunch.x - aimPunch.x * 2.f,
-			viewAngles.y + oldPunch.y - aimPunch.y * 2.f,
-		};
-
-		if (newAngles.x > 89.f)
-			newAngles.x = 89.f;
-
-		if (newAngles.x < -89.f)
-			newAngles.x = -89.f;
-
-		while (newAngles.y > 180.f)
-			newAngles.y -= 360.f;
-
-		while (newAngles.y < -180.f)
-			newAngles.y += 360.f;
-
-		newAngles.x += ScreenCenterX;
-		newAngles.y += ScreenCenterY;
-
-		Angles = newAngles;
-		oldPunch = aimPunch;
-	}
-	else
-	{
-		oldPunch.x = oldPunch.y = 0.f;
-	}
-
-	Vec2 PunchAngle;
-	if (Local.Pawn.AimPunchCache.Count <= 0 && Local.Pawn.AimPunchCache.Count > 0xFFFF)
-		return;
-	if (!memoryManager.ReadMemory<Vec2>(Local.Pawn.AimPunchCache.Data + (Local.Pawn.AimPunchCache.Count - 1) * sizeof(Vec3), PunchAngle))
-		return;
-
-	Angles.x = PunchAngle.x;
-	Angles.y = PunchAngle.y;
+// Implementation namespace-internal config instance
+namespace RCS {
+    Config config;
 }
 
-void RCS::RecoilControl(CEntity LocalPlayer)
+static Vec2 lastPunch{0.f, 0.f};
+
+void RCS::NormalizeAngles(Vec2& angles) {
+    // Clamp pitch
+    if (angles.x > 89.f) angles.x = 89.f;
+    if (angles.x < -89.f) angles.x = -89.f;
+
+    // Normalize yaw
+    while (angles.y > 180.f) angles.y -= 360.f;
+    while (angles.y < -180.f) angles.y += 360.f;
+}
+
+void RCS::UpdateAngles(const CEntity& local, Vec2& outAngles)
 {
-	if (!LegitBotConfig::RCS)
-		return;
+    const int shotsFired = local.Pawn.ShotsFired;
 
-	static Vec2 OldPunch;
+    if (shotsFired > 0)
+    {
+        // Compute corrected aiming angle
+        const Vec2 viewAngles = local.Pawn.ViewAngle;
+        const Vec2 aimPunch = local.Pawn.AimPunchAngle;
+        Vec2 newAngles = {
+            viewAngles.x + lastPunch.x - aimPunch.x * 2.0f,
+            viewAngles.y + lastPunch.y - aimPunch.y * 2.0f
+        };
 
-	if (LocalPlayer.Pawn.ShotsFired > RCSBullet)
-	{
-		Vec2 viewAngles = LocalPlayer.Pawn.ViewAngle;
-		Vec2 delta = viewAngles - (viewAngles + (OldPunch - (LocalPlayer.Pawn.AimPunchAngle * 2.f)));
+        NormalizeAngles(newAngles);
+        outAngles = newAngles;
+        lastPunch = aimPunch;
+    }
+    else
+    {
+        lastPunch = Vec2{0.f, 0.f};
+        outAngles = local.Pawn.ViewAngle;
+    }
 
-        int MouseX = static_cast<int>(std::round((delta.y * RCSScale.x / LocalPlayer.Client.Sensitivity) / 0.011f));
-        int MouseY = static_cast<int>(std::round((delta.x * RCSScale.y / LocalPlayer.Client.Sensitivity) / 0.011f));
+    // Optionally, refine with latest punch value from memory cache
+    // Only if punch cache is valid (defensive check)
+    if (local.Pawn.AimPunchCache.Count > 0 && local.Pawn.AimPunchCache.Count <= 0xFFFF) {
+        Vec2 punchAngle;
+        const uintptr_t offset = local.Pawn.AimPunchCache.Data +
+            (local.Pawn.AimPunchCache.Count - 1) * sizeof(Vec3);
+        if (memoryManager.ReadMemory<Vec2>(offset, punchAngle)) {
+            outAngles = punchAngle;
+        }
+    }
+}
 
-		if ((GetAsyncKeyState(VK_LBUTTON) & 0x8000))
-		{
-			mouse_event(MOUSEEVENTF_MOVE, MouseX, -MouseY, NULL, NULL);
-		}
+void RCS::RecoilControl(const CEntity& local)
+{
+    if (!config.enabled)
+        return;
 
-		OldPunch = LocalPlayer.Pawn.AimPunchAngle * 2.0f;
-	}
-	else
-		OldPunch = Vec2{ 0,0 };
+    static Vec2 prevPunch{0.f, 0.f};
+
+    if (local.Pawn.ShotsFired > config.bulletsToActivate)
+    {
+        Vec2 viewAngles = local.Pawn.ViewAngle;
+        Vec2 punch = local.Pawn.AimPunchAngle * 2.0f;
+
+        Vec2 delta = viewAngles + (prevPunch - punch);
+        Vec2 scaledDelta = {
+            delta.y * config.scale.x / local.Client.Sensitivity,
+            delta.x * config.scale.y / local.Client.Sensitivity
+        };
+
+        int mouseX = static_cast<int>(std::round(scaledDelta.x / 0.011f));
+        int mouseY = static_cast<int>(std::round(scaledDelta.y / 0.011f));
+
+        if ((GetAsyncKeyState(VK_LBUTTON) & 0x8000))
+        {
+            mouse_event(MOUSEEVENTF_MOVE, mouseX, -mouseY, NULL, NULL);
+        }
+
+        prevPunch = punch;
+    }
+    else
+    {
+        prevPunch = Vec2{0.f, 0.f};
+    }
 }
